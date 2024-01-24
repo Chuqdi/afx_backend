75bdda5d4104d68330e100bb5c915d8267184e5a
PiusLucky <luckypius50@gmail.com>
2024-01-24 11:26:24 +0100

    chore: updated afirmations apis

--- src/models/affirmation_listening_history.py
+++ src/models/affirmation_listening_history.py
@@ -8,7 +8,7 @@ from src.models.user import User
 
 class AffirmationListeningHistory(TimeBaseModel):
     user: Optional[Link[User]] = None
-    affirmation: Optional[Affirmation] = None
+    affirmation: Optional[Link[Affirmation]] = None
     start_at: datetime
     resumed_at: datetime
     listened_until: datetime
@@ -16,3 +16,20 @@ class AffirmationListeningHistory(TimeBaseModel):
 
     class Settings:
         name = "affirmation_listening_history"
+        projection = {
+            "user": "$user._id",
+            "affirmation": "$affirmation._id",
+            "resumed_at": 1,
+            "listened_until": 1,
+            "completed": 1,
+        }
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "start_at": datetime.now(),
+                "resumed_at": datetime.now(),
+                "listened_until": datetime.now(),
+                "completed": False,
+            }
+        }
